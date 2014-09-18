5ca31163ec559fda10750a01f810426f325618dc
isuzuki <isuzuki@suzukiikuo-no-MacBook-Air.local>
2014-09-19 03:20:43 +0900

    PHP5.6, 5.5, 5.4を選択できるようにした

--- php/site-cookbooks/php/recipes/default.rb
+++ php/site-cookbooks/php/recipes/default.rb
@@ -1,5 +1,19 @@
-%w{php55u}.each do |p|
-  package p do
+case node[:php][:version]
+when '5.6'
+  include_recipe 'yum-remi::php56'
+  option = '--enablerepo=remi-php56'
+when '5.5'
+  include_recipe 'yum-remi::php55'
+  option = '--enablerepo=remi-php55'
+when '5.4'
+  option = '--enablerepo=remi'
+else
+  raise '未対応のバージョンです'
+end
+
+%w{php}.each do |pkg|
+  package pkg do
+    options option
     action :install
   end
 end
